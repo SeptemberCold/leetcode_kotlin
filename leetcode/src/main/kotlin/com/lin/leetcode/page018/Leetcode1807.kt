580f0791ab7183cf3afb53458ca55ce6fb1aa917
lin <294795817@qq.com>
2023-01-13 09:21:18 +0800

    力扣第1807题:替换字符串中的括号内容-> 添加题目描述

--- leetcode/src/main/kotlin/com/lin/leetcode/page018/Leetcode1807.kt
+++ leetcode/src/main/kotlin/com/lin/leetcode/page018/Leetcode1807.kt
@@ -1,6 +1,16 @@
 package com.lin.leetcode.page018
 
-
+/**
+ * 替换字符串中的括号内容
+ * 给你一个字符串s，它包含一些括号对，每个括号中包含一个 非空的键。
+ * 比方说，字符串"(name)is(age)yearsold"中，有两个括号对，分别包含键"name" 和"age"。
+ * 你知道许多键对应的值，这些关系由二维字符串数组knowledge表示，其中knowledge[ i ] = [key i, value i]，表示键key i对应的值为value i。
+ * 你需要替换 所有的括号对。当你替换一个括号对，且它包含的键为keyi时，你需要：
+ * 将key i和括号用对应的值value i替换。
+ * 如果从 knowledge中无法得知某个键对应的值，你需要将key i和括号用问号"?"替换（不需要引号）.
+ * knowledge中每个键最多只会出现一次。s中不会有嵌套的括号。
+ * 请你返回替换 所有括号对后的结果字符串。
+ */
 object Leetcode1807 {
 
     /**
