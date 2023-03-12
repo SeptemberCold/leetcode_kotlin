7d31da52453548c8ca1b9929ed38249d9ecf0902
29479 <294795817@qq.com>
2023-03-12 22:34:38 +0800

    LeetcodeBaseTest->注释相似方法，用于后续测试拷贝

--- LeetcodeKotlin/test/base/LeetcodeBaseTest.kt
+++ LeetcodeKotlin/test/base/LeetcodeBaseTest.kt
@@ -7,10 +7,25 @@ package base
  */
 abstract class LeetcodeBaseTest<EXAMPLE, RESULT> {
 
+//    @JvmStatic
+//    fun main(args: Array<String>) {
+//        val example = LeetcodeTest119.getExample()
+//        LeetcodeTest119.methodTest(example) { return@methodTest Leetcode119.getRow(it) }
+//    }
+
     /**
      * 调用测试方法
      */
     abstract fun methodTest(example: List<EXAMPLE>, method: (x: EXAMPLE) -> RESULT)
+
+//    private fun methodTest(example: List<Int>, method: (x: Int) -> Boolean) {
+//        example.forEach {
+//            val isPalindrome = method(it)
+//            println("输入:x=${it}")
+//            println("输出:${isPalindrome}")
+//            println("====================")
+//        }
+//    }
 
     /**
      * 获取范例的方法
