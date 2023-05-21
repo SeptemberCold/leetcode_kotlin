e99c668b29b798defef008a239f1112681a7e824
29479 <294795817@qq.com>
2023-05-21 23:54:35 +0800

    基本测试类修改

--- LeetcodeKotlin/test/base/LeetcodeBaseTest2.kt
+++ LeetcodeKotlin/test/base/LeetcodeBaseTest2.kt
@@ -6,10 +6,35 @@ abstract class LeetcodeBaseTest2<EXAMPLE, RESULT> : LeetcodeBaseTest<EXAMPLE, RE
     override fun methodTest(example: List<EXAMPLE>, method: (x: EXAMPLE) -> RESULT) {
         example.forEach {
             val result = method(it)
-            println("输入:x=${it}")
-            println("输出:${result}")
+            if (it is IntArray) {
+                println("输入:x=${intArrayToString(it)}")
+            } else {
+                println("输入:x=${it}")
+            }
+
+            if (result is IntArray) {
+                println("输出:${intArrayToString(result)}")
+            } else {
+                println("输出:${result}")
+            }
             println("====================")
         }
+    }
+
+    /**
+     * int数组转string
+     */
+    private fun intArrayToString(intArray: IntArray): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append("[")
+        for (i in intArray.indices) {
+            if (i > 0) {
+                stringBuilder.append(",")
+            }
+            stringBuilder.append(intArray[i])
+        }
+        stringBuilder.append("]")
+        return stringBuilder.toString()
     }
 
 
