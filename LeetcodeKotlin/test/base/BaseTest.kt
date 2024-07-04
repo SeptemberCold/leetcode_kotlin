cf87024dc80c3b0c66b8d57bb533c211006c4b64
时寒很苦恼 <294795817@qq.com>
2024-07-04 16:31:23 +0800

    基础测试类增加测试方法

--- LeetcodeKotlin/test/base/BaseTest.kt
+++ LeetcodeKotlin/test/base/BaseTest.kt
@@ -5,4 +5,8 @@ abstract class BaseTest<EXAMPLE, RESULT> : LeetcodeBaseTest2<EXAMPLE, RESULT>() 
 
     fun getArrayList() = ArrayList<EXAMPLE>()
 
+    fun test(method: (x: EXAMPLE) -> RESULT) {
+        methodTest(getExample()) { return@methodTest method(it) }
+    }
+
 }
